d1409a9ea18a730f48a6e7186c17172b1e43c269
Aleksey Goravskiy <dement_box@inbox.ru>
2019-02-19 20:03:37 +0300

    Refactoring

--- ArrayHelper.cs
+++ ArrayHelper.cs
@@ -6,21 +6,18 @@ namespace PadawansTask5
     {
         public static string CheckIfSymmetric(int[] source)
         {
-            if (source.Length == 0 || source.Length == 1)
+            if (source == null)
             {
-                throw new ArgumentException();
+                throw new ArgumentNullException();
             }
-            int counter = 0;
-            int j = source.Length - 1;
-            for (int i = 0; i < source.Length / 2; i++)
+            for (int i = 0, j = source.Length - 1; i < source.Length / 2; i++, j--)
             {
-                if (source[i] == source[j])
+                if (source[i] != source[j])
                 {
-                    counter++;
+                    return "No";
                 }
-                j--;
             }
-            return counter == source.Length / 2 ? "Yes" : "No";
+            return "Yes";
         }
     }
 }

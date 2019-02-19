339bc9cab1241dbd365d5372921cf76b13b372cb
Aleksey Goravskiy <dement_box@inbox.ru>
2019-02-19 16:48:16 +0300

    Initial commit

--- ArrayHelper.cs
+++ ArrayHelper.cs
@@ -6,8 +6,21 @@ namespace PadawansTask5
     {
         public static string CheckIfSymmetric(int[] source)
         {
-            // put your code here
-            throw new NotImplementedException();
+            if (source.Length == 0 || source.Length == 1)
+            {
+                throw new ArgumentException();
+            }
+            int counter = 0;
+            int j = source.Length - 1;
+            for (int i = 0; i < source.Length / 2; i++)
+            {
+                if (source[i] == source[j])
+                {
+                    counter++;
+                }
+                j--;
+            }
+            return counter == source.Length / 2 ? "Yes" : "No";
         }
     }
 }

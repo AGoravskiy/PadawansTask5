e8a1a7653b4f97cfb91e6e7297375a1062c5b590
Aleksey Goravskiy <dement_box@inbox.ru>
2019-02-19 20:07:41 +0300

    Add ArgumentException

--- ArrayHelper.cs
+++ ArrayHelper.cs
@@ -10,6 +10,10 @@ namespace PadawansTask5
             {
                 throw new ArgumentNullException();
             }
+            if (source.Length == 0)
+            {
+                throw new ArgumentException();
+            }
             for (int i = 0, j = source.Length - 1; i < source.Length / 2; i++, j--)
             {
                 if (source[i] != source[j])
